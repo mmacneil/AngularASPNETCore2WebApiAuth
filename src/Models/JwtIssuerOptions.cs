7b630210b047f1f97c7252ae3034df48e7d9d8ef
mmacneil <mark.macneil@t4g.com>
2018-02-20 11:24:03 -0400

    Fixed expiration error due caused by static IssuedAt datetime val.

--- src/Models/JwtIssuerOptions.cs
+++ src/Models/JwtIssuerOptions.cs
@@ -1,4 +1,4 @@
-﻿
+
 
 using System;
 using System.Threading.Tasks;
@@ -31,12 +31,12 @@ namespace AngularASPNETCore2WebApiAuth.Models
         /// <summary>
         /// 4.1.5.  "nbf" (Not Before) Claim - The "nbf" (not before) claim identifies the time before which the JWT MUST NOT be accepted for processing.
         /// </summary>
-        public DateTime NotBefore { get; set; } = DateTime.UtcNow;
+        public DateTime NotBefore => DateTime.UtcNow;
 
         /// <summary>
         /// 4.1.6.  "iat" (Issued At) Claim - The "iat" (issued at) claim identifies the time at which the JWT was issued.
         /// </summary>
-        public DateTime IssuedAt { get; set; } = DateTime.UtcNow;
+        public DateTime IssuedAt => DateTime.UtcNow;
 
         /// <summary>
         /// Set the timespan the token will be valid for (default is 120 min)
